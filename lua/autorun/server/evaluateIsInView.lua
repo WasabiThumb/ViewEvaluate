b817a48f0b2a2b54a84e06a6136f81a4426262c2
Wasabi_Thumbs <wasabithumbs@gmail.com>
2018-10-17 10:06:19 -0400

    Updated the lua

--- lua/autorun/server/evaluateIsInView.lua
+++ lua/autorun/server/evaluateIsInView.lua
@@ -1,8 +1,17 @@
 -- Is an NPC lookins at u?
 
 function checkIsInView(player, npc, fov)
-  if !IsValid(npc) or !IsValid(npc) then return end
-  if !npc:IsNPC() then return end
+  if !IsValid(npc) or !IsValid(npc) then return false end
+  if !npc:IsNPC() then return false end
+  if !player:IsPlayer() then return false end
   if !fov then local fov = player:GetFOV() end
-  
+  local dir = npc:GetAimVector():GetNormalized()
+  local preliminary = util.TraceLine({
+      start = npc:EyePos(),
+      endpos = player:GetPos() + player:GetForward()*-50 + player:GetUp()*12,
+      filter = {npc}
+    })
+  if !(preliminary.Entity == player) then return false end
+  local thoseinview = ents.FindInCone(npc:EyePos(), dir, 261888, (fov/2))
+  if table.HasValue(thoseinview, player) then return true end else return false end
 end

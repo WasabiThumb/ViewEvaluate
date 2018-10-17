d79a64fb009839ec50f0f22ffefffc85ee909de0
Wasabi_Thumbs <wasabithumbs@gmail.com>
2018-10-17 10:15:34 -0400

    Create reportLumocity.lua

---
+++ lua/autorun/client/reportLumocity.lua
@@ -0,0 +1,6 @@
+-- reports lumocity from client
+
+net.Receive("gimmeLumocityPlox", function(len, ply)
+  local luminocity = math.Clamp((render.GetLightColor(LocalPlayer():GetPos())*Vector(255,255,255)):Length()+ ((LocalPlayer():FlashlightIsOn() and 1 or 0)*100),0,255)
+  
+end )

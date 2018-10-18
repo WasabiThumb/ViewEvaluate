634794c0e87e653a9884c106125525957d41f201
Wasabi_Thumbs <wasabithumbs@gmail.com>
2018-10-18 11:25:53 -0400

    Update evaluateIsInView.lua

--- lua/autorun/server/evaluateIsInView.lua
+++ lua/autorun/server/evaluateIsInView.lua
@@ -1,6 +1,11 @@
 -- Is an NPC lookins at u?
 local range = 26000
 local defaultMinBrightness = 0.75
+local checkFrequencyMS = 500
+local npcOmitList = {}
+local npcExceptions = {}
+local NPCFovExceptions = {}
+local includeSents = false;
 
 function checkIsInView(player, npc, fov)
   if !IsValid(npc) or !IsValid(npc) then return false end
@@ -23,3 +28,25 @@ function checkIsVisibleToNPC(player, npc, lumosity)
   if npcExceptions[npc:GetClass()] then npcMin = npcExceptions[npc:GetClass()] end
   if lumosity >= npcMin then return true else return false end
 end
+
+hook.Add("InitPostEntity", "startNPCCheckTimer", function()
+  timer.Create("PerNPCVisibilityCheck", checkFrequencyMS, 0, function()
+    local npcs = ents.FindByClass("npc_*")
+    if includesents then
+      local sents = ents.FindByClass("sent_*")
+      table.Add(npcs, sents)
+    end
+    for _,v in pairs(npcs) do
+      if !IsValid(v) then continue end
+      for _,p in pairs(ents.FindInSphere(v:GetPos(), range)) do
+        if !IsValid(p) then continue end
+        if !p:IsPlayer() then continue end
+        local fov = nil;
+        if NPCFovExceptions[v] then fov = NPCFovExceptions[v] end
+        if checkIsInView(p, v, fov) then
+          -- Do luminosity check here
+        end
+      end
+    end
+  end )
+end )

0a970d483254e24389edc98a66fd20ddf6dad6fb
Wasabi_Thumbs <wasabithumbs@gmail.com>
2018-10-18 11:00:00 -0400

    Add response to client luminosity report

--- lua/autorun/client/reportLumocity.lua
+++ lua/autorun/client/reportLumocity.lua
@@ -2,5 +2,7 @@
 
 net.Receive("gimmeLumocityPlox", function(len, ply)
   local luminocity = math.Clamp((render.GetLightColor(LocalPlayer():GetPos())*Vector(255,255,255)):Length()+ ((LocalPlayer():FlashlightIsOn() and 1 or 0)*100),0,255)
-  
+  net.Start("okLumocityBaka")
+  net.WriteInt(lumocity, 32)
+  net.SendToServer()
 end )

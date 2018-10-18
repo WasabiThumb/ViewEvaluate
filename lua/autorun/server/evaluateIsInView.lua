ab92aac80dae25bd69962fc2784f6a1dd1726e9a
Wasabi_Thumbs <wasabithumbs@gmail.com>
2018-10-18 11:10:01 -0400

    Update evaluateIsInView.lua

--- lua/autorun/server/evaluateIsInView.lua
+++ lua/autorun/server/evaluateIsInView.lua
@@ -1,4 +1,6 @@
 -- Is an NPC lookins at u?
+local range = 26000
+local defaultMinBrightness = 0.75
 
 function checkIsInView(player, npc, fov)
   if !IsValid(npc) or !IsValid(npc) then return false end
@@ -8,10 +10,16 @@ function checkIsInView(player, npc, fov)
   local dir = npc:GetAimVector():GetNormalized()
   local preliminary = util.TraceLine({
       start = npc:EyePos(),
-      endpos = player:GetPos() + player:GetForward()*-50 + player:GetUp()*12,
+      endpos = player:GetPos() + player:GetForward()*-1 + player:GetUp()*-1,
       filter = {npc}
     })
   if !(preliminary.Entity == player) then return false end
-  local thoseinview = ents.FindInCone(npc:EyePos(), dir, 261888, (fov/2))
+  local thoseinview = ents.FindInCone(npc:EyePos(), dir, range, (fov/2))
   if table.HasValue(thoseinview, player) then return true end else return false end
+end
+
+function checkIsVisibleToNPC(player, npc, lumosity)
+  local npcMin = defaultMinBrightness
+  if npcExceptions[npc:GetClass()] then npcMin = npcExceptions[npc:GetClass()] end
+  if lumosity >= npcMin then return true else return false end
 end
